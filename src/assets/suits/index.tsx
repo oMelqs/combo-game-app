010b7ed28fd7cd03dcefb4f255c2cdfb15c0ada5
oMelqs <melquisedeque.linhares@gmail.com>
2023-01-06 22:43:30 -0300

    feat: add index of suits svgs

---
+++ src/assets/suits/index.tsx
@@ -0,0 +1,4 @@
+export { default as Club } from "@assets/suits/club-suit.svg";
+export { default as Diamond } from "@assets/suits/diamond-suit.svg";
+export { default as Heart } from "@assets/suits/heart-suit.svg";
+export { default as Spade } from "@assets/suits/spade-suit.svg";
